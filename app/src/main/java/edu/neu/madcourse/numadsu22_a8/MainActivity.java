8f1af0de87eabc6e9f96aef89754483ad5ae0c28
icodealittle <csoumeng@gmail.com>
2022-07-09 14:27:07 -0400

    TODO: Sticker Display.java

--- app/src/main/java/edu/neu/madcourse/numadsu22_a8/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numadsu22_a8/MainActivity.java
@@ -11,6 +11,19 @@ Color pallet 1:
     - #72A1E5
     - #9883E5
     - #FCD3DE
+
+    Link: https://coolors.co/2e382e-50c9ce-72a1e5-9883e5-fcd3de
+ */
+
+/*
+Color Pallet 2:
+    - #26547C
+    - #EF476F
+    - #FFD166
+    - #06D6A0
+    - #FFFCF9
+
+    Link: https://coolors.co/26547c-ef476f-ffd166-06d6a0-fffcf9
  */
 public class MainActivity extends AppCompatActivity {
 
